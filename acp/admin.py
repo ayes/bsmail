42e8c06d4c856b87bedd26c31e1df5d4e9441d4c
WST <admin@jsmart.web.id>
2013-12-23 15:04:41 +0400

    Fixes requested by ayes

--- acp/admin.py
+++ acp/admin.py
@@ -6,7 +6,11 @@ from django.contrib.auth.models import Group
 from models import *
 
 class MailDomainAdmin(admin.ModelAdmin):
-	list_display = ('__unicode__', 'notes')
+	list_display = ('__unicode__', 'notes', 'position')
+	list_editable = ('position',)
+
+	class Media:
+		js = ['/static/js/admin_list_reorder.js',]
 
 class MailUserAdmin(admin.ModelAdmin):
 	list_display = ('__unicode__', 'domain', 'mailbox_size', 'active')
--- acp/models.py
+++ acp/models.py
@@ -6,6 +6,7 @@ from django.forms import ValidationError
 class MailDomain(models.Model):
 	domain = models.CharField(u'Domain name', max_length = 64, help_text = 'Domain name to serve (example: bsmsite.com)', unique = True)
 	notes = models.CharField(u'Notes', max_length = 1024, help_text = 'Anything about this domain')
+	position = models.IntegerField(u'Position', help_text = 'Position in the drop-down list in mail.bsmsite.com')
 
 	def __unicode__(self):
 		return self.domain
@@ -14,7 +15,19 @@ class MailDomain(models.Model):
 		db_table = 'domains'
 		verbose_name = 'mail domain'
 		verbose_name_plural = 'mail domains'
-		ordering = ('domain',)
+		ordering = ('position',)
+
+class MailQuota(models.Model):
+	quota = models.IntegerField('Quota, megabytes', help_text = u'User quota in megabytes, 0 means unlimited')
+	title = models.CharField('Quota name', help_text = 'Example: 100 megabytes, for staff', max_length = 255)
+
+	def __unicode__(self):
+		return self.title
+
+	class Meta:
+		db_table = 'quotas'
+		verbose_name = 'Mail quota'
+		verbose_name_plural = 'Mail quotas'
 
 class MailUser(models.Model):
 	QUOTA_CHOICES = (
@@ -34,7 +47,7 @@ class MailUser(models.Model):
 	username = models.CharField('Username', max_length = 64, help_text = 'Left part (before @ sign) of the e-mail address')
 	domain = models.ForeignKey(MailDomain, verbose_name = 'Domain')
 	password = models.CharField('Password', max_length = 32, help_text = 'Used to connect through POP, IMAP and SMTP')
-	quota = models.IntegerField('Mailbox size', choices = QUOTA_CHOICES, help_text = 'How much space should the user have?')
+	quota = models.ForeignKey(MailQuota, verbose_name = 'Mailbox size')
  	active = models.BooleanField('Access', help_text = 'Disabling the user’s access does not destroy his mailbox')
 
 	class Meta:
