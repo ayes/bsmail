22f3e3a026c7a1dba9507806f10551bab6e0e9af
WST <ilya.averkov@gmail.com>
2013-12-23 11:33:24 +0000

    Fixes

--- acp/admin.py
+++ acp/admin.py
@@ -16,7 +16,11 @@ class MailUserAdmin(admin.ModelAdmin):
 	list_display = ('__unicode__', 'domain', 'mailbox_size', 'active')
 	list_filter = ('domain',)
 
+class MailQuotaAdmin(admin.ModelAdmin):
+	pass
+
 admin.site.register(MailDomain, MailDomainAdmin)
 admin.site.register(MailUser, MailUserAdmin)
+admin.site.register(MailQuota, MailQuotaAdmin)
 
 admin.site.unregister(Group)
